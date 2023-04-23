775958b150d4abb224150bd214bea94bfb7765b5
Emanuel <marquezema127@gmail.com>
2023-04-23 13:00:24 -0300

    modified cors to deploy

--- PI-Food-main/api/src/app.js
+++ PI-Food-main/api/src/app.js
@@ -15,7 +15,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   //res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'false');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
