3296544840ae286e4d2902f14dca1dc023974e63
Emanuel <marquezema127@gmail.com>
2023-04-23 12:52:21 -0300

    modified cors to deploy

--- PI-Food-main/api/src/app.js
+++ PI-Food-main/api/src/app.js
@@ -14,7 +14,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   //res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Origin', 'https://pi-foodie-app.vercel.app/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
